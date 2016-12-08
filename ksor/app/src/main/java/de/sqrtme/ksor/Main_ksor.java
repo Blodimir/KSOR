889a90deb87613790f095e7661d2a1a77de051db
Sqrtme <Caps2073@gmail.com>
2016-12-08 18:32:48 +0300

    1

---
+++ ksor/app/src/main/java/de/sqrtme/ksor/Main_ksor.java
@@ -0,0 +1,13 @@
+package de.sqrtme.ksor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_ksor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_ksor);
+    }
+}
